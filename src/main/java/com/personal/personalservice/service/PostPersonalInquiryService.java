dd3049df37ceb499ca5852f1ef4ba1b1229384ba
bobby setiawan <abobbysetiawan@gmail.com>
2026-07-12 03:38:35 +0700

    initial commit for adding redis connection and function key

--- src/main/java/com/personal/personalservice/service/PostPersonalInquiryService.java
+++ src/main/java/com/personal/personalservice/service/PostPersonalInquiryService.java
@@ -4,6 +4,7 @@ import com.personal.personalservice.model.request.PostPersonalInquiryRequest;
 import com.personal.personalservice.model.response.PostPersonalInquiryResponse;
 import com.personal.personalservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class PostPersonalInquiryService {
 
     private final UserRepository userRepository;
+    private final StringRedisTemplate stringRedisTemplate;
 
     public PostPersonalInquiryResponse execute(PostPersonalInquiryRequest request) {
         return userRepository.findByUsername(request.getName())
@@ -18,6 +20,8 @@ public class PostPersonalInquiryService {
                         .name(u.getFullName())
                         .email(u.getEmail())
                         .status(u.getStatus())
+                        .hasSession(stringRedisTemplate.opsForValue()
+                                .getOperations().hasKey("user-token-".concat(u.getEmail())))
                         .build())
                 .orElse(new PostPersonalInquiryResponse());
     }
