6c11556fef485212d749c18018bdf10c17b24845
bobby setiawan <abobbysetiawan@gmail.com>
2026-08-14 00:19:00 +0700

    add log

--- src/main/java/com/personal/personalservice/controller/PersonalController.java
+++ src/main/java/com/personal/personalservice/controller/PersonalController.java
@@ -4,12 +4,10 @@ import com.personal.personalservice.model.request.PostPersonalInquiryRequest;
 import com.personal.personalservice.model.response.PostPersonalInquiryResponse;
 import com.personal.personalservice.service.PostPersonalInquiryService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Log4j2
 @RestController
 @RequiredArgsConstructor
 public class PersonalController {
@@ -18,7 +16,6 @@ public class PersonalController {
 
     @PostMapping("/personal/v1/inquiry")
     public PostPersonalInquiryResponse personalInquiry(@RequestBody PostPersonalInquiryRequest request) {
-        log.info("Inquiry data");
         return postPersonalInquiryService.execute(request);
     }
 }
--- src/main/java/com/personal/personalservice/service/PostPersonalInquiryService.java
+++ src/main/java/com/personal/personalservice/service/PostPersonalInquiryService.java
@@ -4,9 +4,11 @@ import com.personal.personalservice.model.request.PostPersonalInquiryRequest;
 import com.personal.personalservice.model.response.PostPersonalInquiryResponse;
 import com.personal.personalservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class PostPersonalInquiryService {
@@ -15,6 +17,7 @@ public class PostPersonalInquiryService {
     private final StringRedisTemplate stringRedisTemplate;
 
     public PostPersonalInquiryResponse execute(PostPersonalInquiryRequest request) {
+        log.info("Inquiry data");
         return userRepository.findByUsername(request.getName())
                 .map(u -> PostPersonalInquiryResponse.builder()
                         .name(u.getFullName())
